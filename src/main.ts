f4d5d99a2117258d7432125a45dd1c8b00d0f453
Marvin Wendt <github@marvinjwendt.com>
2019-09-07 03:23:04 +0200

    Update

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ const exec = require('@actions/exec');
 async function run() {
   try {
     await exec.exec('npm run lint');
+    await exec.exec('git commit -m "Linted file"')
+    await exec.exec('git push')
   } catch (error) {
     core.setFailed(error.message);
   }
